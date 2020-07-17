b1b3e24741bc646b29f32ea486ec94b7a841f87d
dawsonhammdev <hammsdesigns@gmail.com>
2020-07-17 10:38:46 -0500

    ring buffer tests are now passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None] * capacity
+        self.current = 0
+
+# we would start like this: [None None None None]
 
     def append(self, item):
-        pass
-
+        # set item to equal the current in storage
+        self.storage[self.current] = item
+        # append 1 on to current
+        self.current += 1
+        # if current is equal to capacity then current is zero
+        if self.current == self.capacity:
+            self.current = 0
     def get(self):
-        pass
+        # return a for loop for all the items in storage that have a value
+        return [x for x in self.storage if x is not None]
